5313d7859ab90a7249664538c56800784acaf0ef
MdAfsarHossain <afsar291722@gmail.com>
2023-10-09 20:33:45 +0600

    CRUD Function Based code added

---
+++ 3. CRUD Function Based/gs3/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Student
+
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+    
+
+    def create(self, validation_data):
+        return Student.objects.create(**validation_data)
+    
+    
+    def update(self, instance, validation_data):
+        print(instance.name)
+        instance.name = validation_data.get('name', instance.name)
+        print(instance.name)
+        instance.roll = validation_data.get('roll', instance.roll)
+        instance.city = validation_data.get('city', instance.city)
+        instance.save()
+        return instance
+        
